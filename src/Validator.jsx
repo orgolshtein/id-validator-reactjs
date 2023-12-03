c7cc2e8484008e053987b58ef4ca7bdf5ae18ff7
orgolshtein <129406884+orgolshtein@users.noreply.github.com>
2023-12-03 22:39:23 +0200

    Update Validator.jsx

--- src/Validator.jsx
+++ src/Validator.jsx
@@ -25,35 +25,38 @@ export default function Validator({input, output, setOutput}){
    },[input])
 
     return (
-        <div>
+        <ValidatorDiv>
             {isNaN(Number(input))=== true ?
-            (<H3 textcolor={red}>יש להקליד מספרים בלבד</H3>)
+            (<H3 $textcolor={red}>יש להקליד מספרים בלבד</H3>)
             : input.length === 9 && (output+sum) % 10 === 0 ?
-            (<H2 textcolor={green}>מספר הזהות {zeroPad(input,9)} תקין</H2>)
+            (<H2 $textcolor={green}>מספר הזהות {zeroPad(input,9)} תקין</H2>)
             : input.length === 9 && (output+sum) % 10 !== 0 ?
-            (<H2 textcolor={red}>מספר הזהות {zeroPad(input,9)} לא תקין</H2>)
+            (<H2 $textcolor={red}>מספר הזהות {zeroPad(input,9)} לא תקין</H2>)
             : input.length === 0 ?
-            (<H3 textcolor={yellow}>הקלד/י עד 8 ספרות כדי לקבל את ספרת הביקורת<br />או הקלד/י 9 ספרות כדי לאמת את מספר הזהות המלא</H3>)
+            (<H3 $textcolor={yellow}>הקלד/י עד 8 ספרות כדי לקבל את ספרת הביקורת<br />או הקלד/י 9 ספרות כדי לאמת את מספר הזהות המלא</H3>)
             : (
             <>
-            <H3 textcolor={yellow}>ספרת הביקורת עבור {zeroPad(input,8)} היא</H3>
-            <H2 textcolor={blue}>{output}</H2>
+            <H3 $textcolor={yellow}>ספרת הביקורת עבור {zeroPad(input,8)} היא</H3>
+            <H2 $textcolor={blue}>{output}</H2>
             </>
             )}
-        </div>
+        </ValidatorDiv>
     );
 };
+
+const ValidatorDiv = styled.div`
+  min-height: 100px;  
+`;
+
+const H2 = styled.h2`
+    color: ${(props) => props.$textcolor};
+`;
+
+const H3 = styled.h3`
+    color: ${(props) => props.$textcolor};
+`;
 
 const blue = "Blue";
 const red = "Red";
 const green = "Green";
 const yellow = "Yellow";
-
-const H2 = styled.h2`
-    color: ${(props) => props.textcolor};
-`;
-
-const H3 = styled.h3`
-    color: ${(props) => props.textcolor};
-`;
-
